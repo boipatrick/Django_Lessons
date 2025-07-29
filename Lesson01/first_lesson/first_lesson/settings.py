4b7b49c8c3ca3472a99dd77e84a4617a4cfbf703
boipatrick <boipatrick@protonmail.com>
2025-07-29 10:38:43 +0300

    lesson02

--- Lesson01/first_lesson/first_lesson/settings.py
+++ Lesson01/first_lesson/first_lesson/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'posts',  # Ensure 'posts' app is included
 ]
 
 MIDDLEWARE = [
---
+++ Lesson01/first_lesson/posts/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=75)
+    body = models.TextField()
+    slug = models.SlugField()
+    date = models.DateTimeField(auto_now_add=True) # Automatically set the date when the post is created"
+
+    def __str__(self):
+        return self.title
